504ee4d1b5348df848f3143b84d33968e5662ccb
Muratcan Payir <muratcanpayir@gmail.com>
2021-12-09 20:11:10 +0000

    Order modal added

--- client/src/pages/Cart/index.js
+++ client/src/pages/Cart/index.js
@@ -1,9 +1,28 @@
-import { Button, Alert, Image, Box, Text } from "@chakra-ui/react";
-import React from "react";
+import {
+  Button,
+  Alert,
+  Image,
+  Box,
+  Text,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input
+} from "@chakra-ui/react";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../../context/CartContext";
 
 function Cart() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = useRef();
   const { items, removeFromCart } = useCart();
   const total = items.reduce((acc, obj) => acc + obj.price, 0);
   return (
@@ -13,7 +32,7 @@ function Cart() {
       )}
       {items.length > 0 && (
         <>
-          <ul style={{listStyleType:"decimal"}}> 
+          <ul style={{ listStyleType: "decimal" }}>
             {items.map((item) => (
               <li key={item._id} style={{ marginBottom: 15 }}>
                 <Link to={`/product/${item._id}`}>
@@ -40,8 +59,33 @@ function Cart() {
           <Box mt="10">
             <Text fontSize="22">Total: {total} TL</Text>
           </Box>
+          <Button mt="2" size="sm" colorScheme="green" onClick={onOpen}>
+            Order
+          </Button>
+          <Modal
+        initialFocusRef={initialRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create your account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>First name</FormLabel>
+              <Input ref={initialRef} placeholder='First name' />
+            </FormControl>
+          </ModalBody>
 
-          <Button mt="2" size="sm" colorScheme="green">Order</Button>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
         </>
       )}
     </Box>
