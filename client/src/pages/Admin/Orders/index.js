ca8eaf2fba24e01a4100dad3a170c8858f8e6e0c
Muratcan Payir <muratcanpayir@gmail.com>
2021-12-14 21:20:12 +0000

    React query created

--- client/src/pages/Admin/Orders/index.js
+++ client/src/pages/Admin/Orders/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "react-query";
+import { getOrders } from "../../../helpers/api";
 
 function Orders() {
-  return (
-    <div>
-      Admin Orders
-    </div>
-  )
+  const { isLoading, isError, data, error } = useQuery(
+    "admin:orders",
+    getOrders
+  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error {error}</div>;
+  }
+  return <div>Admin Orders</div>;
 }
 
-export default Orders
+export default Orders;
